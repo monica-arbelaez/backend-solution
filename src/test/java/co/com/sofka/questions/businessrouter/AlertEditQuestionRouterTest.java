94f7f64f673f3688dd8aab461ba9eb30f9c937fe
monica Arbelaez <moniarbe233@gmail.com>
2021-07-09 15:37:16 -0500

    Se hace el test AlertEditQuestionRouter

--- src/test/java/co/com/sofka/questions/businessrouter/AlertEditQuestionRouterTest.java
+++ src/test/java/co/com/sofka/questions/businessrouter/AlertEditQuestionRouterTest.java
@@ -2,14 +2,22 @@ package co.com.sofka.questions.businessrouter;
 
 
 import co.com.sofka.questions.businessusecase.AlertEditQuestionUseCase;
+import co.com.sofka.questions.model.ResponseDTO;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+
+
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +33,26 @@ class AlertEditQuestionRouterTest {
     private WebTestClient webTestClient;
     @MockBean
     private AlertEditQuestionUseCase alertEditQuestionUseCase;
+
+    @Test
+    @DisplayName("Test de alerta de edicion de pregunta")
+    public void AlertEditQuestionRouterTest(){
+        var respomseDTO= new ResponseDTO();
+        respomseDTO.setEstado(false);
+        respomseDTO.setMensaje("Puedes modificar la pregunta");
+        Mockito.when(alertEditQuestionUseCase.throwAlert("1")).thenReturn(Mono.just(respomseDTO));
+
+        webTestClient.get().uri("/alertedit/{id}",1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(ResponseDTO.class)
+                .value(response->{
+                    Assertions.assertThat(response).isInstanceOf(ResponseDTO.class);
+                    Assertions.assertThat(response.getMensaje()).isEqualTo("Puedes modificar la pregunta");
+
+                });
+
+    }
 
 
 }
