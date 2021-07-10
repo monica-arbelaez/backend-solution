7e3dc08440149ec756ae49f773b4b44e41d15c62
monica Arbelaez <moniarbe233@gmail.com>
2021-07-09 21:06:33 -0500

    Se creo el servicio de ListRecommendedQuestions

---
+++ src/main/java/co/com/sofka/questions/businessusecase/ListRecommendedQuestionsUseCase.java
@@ -0,0 +1,29 @@
+package co.com.sofka.questions.businessusecase;
+
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.mapper.QuestionMapper;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ListRecommendedQuestionsUseCase {
+    private final QuestionRepository questionRepository;
+    private final QuestionMapper questionMapper;
+
+    @Autowired
+    public ListRecommendedQuestionsUseCase(QuestionRepository questionRepository, QuestionMapper questionMapper) {
+        this.questionRepository = questionRepository;
+        this.questionMapper = questionMapper;
+    }
+
+    public Flux<QuestionDTO> seek(String question){
+        return   questionRepository.findByquestionLike(question)
+                .map(questionMapper.mapQuestionToDTO());
+
+
+    }
+}
